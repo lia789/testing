e71a38fad16c387b4d1159a04d500f44b915a866
lia789 <liahabib53@gmail.com>
2023-01-27 17:11:14 +0600

    Prothomalo corn project complete

--- prothomalo google sheet con script/prothomalo_scraper/settings.py
+++ prothomalo google sheet con script/prothomalo_scraper/settings.py
@@ -86,4 +86,14 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-# LOG_ENABLED = False
+
+
+# Logs setup
+LOG_FILE = "logs.log"
+LOG_FILE_APPEND = False 
+
+
+
+
+
+
