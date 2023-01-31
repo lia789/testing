b373bdc9d70725e1054287ef1a4f86d1387202df
lia789 <liahabib53@gmail.com>
2023-01-31 08:58:12 +0600

    __@1_Quotes_Web application building project started

--- my_site/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import QuotesText
-
-
-@admin.register(QuotesText)
-class QuotesTextAdmin(admin.ModelAdmin):
-    list_display = ["title", "author"]
--- my_site/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import UserInput
-
-
-class UserInputForm(forms.ModelForm):
-    class Meta:
-        model = UserInput
-        fields = "__all__"
