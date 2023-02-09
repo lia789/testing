87d860ec2273dcc3ddf592bdca6c8e7eee895413
lia789 <liahabib53@gmail.com>
2023-02-09 12:28:17 +0600

    @3 Views function code added

--- youtube_application/youtube_website/data_mining_app/views.py
+++ youtube_application/youtube_website/data_mining_app/views.py
@@ -1,54 +1,58 @@
 from django.shortcuts import render, redirect
-
-from .forms import ChannelSearchKeywordForm
-from .utils import get_channel_info
 import csv
 from django.http import HttpResponse
 
+from .forms import ChannelSearchKeywordForm
+from .utils import get_channel_info
+
+
+
 
 def channels_meta_data(request):
+    """
+    Handle the request for channel data search.
+    If the request method is 'POST', the form data is processed and the `data_preview` view is called.
+    Otherwise, a blank form is displayed.
+
+    :param request: the incoming request object
+    :return: a response object
+    """
+
+
     if request.method == "POST":
-        keyword = request.POST.get("channel_keyword")
-        channel_search_form = ChannelSearchKeywordForm(request.POST)
-
-        if channel_search_form.is_valid():
-            keyword = channel_search_form.cleaned_data.get("channel_keyword")
+        # process the form data
+        form = ChannelSearchKeywordForm(request.POST)
+        if form.is_valid():
+            keyword = form.cleaned_data["channel_keyword"]
             data = get_channel_info(my_input=keyword, max_results=5)
-            request.session["data"] = {}
             request.session["data"] = data
-
             return redirect("data_preview_url")
-
     else:
-        channel_search_form = ChannelSearchKeywordForm()
-        return render(request, "data_mining_app/home_page.html", {"form": channel_search_form})
-
+        # display a blank form
+        form = ChannelSearchKeywordForm()
+    return render(request, "data_mining_app/home_page.html", {"form": form})
 
 
 
 def data_preview(request):
-    data = request.session.get("data", {})
+    """
+    Handle the request for previewing the channel data.
+    If the request method is 'POST', the data is converted to a CSV file and downloaded.
+    Otherwise, the data is displayed.
 
+    :param request: the incoming request object
+    :return: a response object
+    """
+
+    data = request.session.get("data", {})
     if request.method == "POST":
-        # Convert data to CSV file
+        # convert data to CSV file
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = "attachment; filename=data.csv"
-
         writer = csv.writer(response)
         writer.writerow(["Number", "Title", "Description", "Published"])
         for i, item in data.items():
             writer.writerow([i, item["title"], item["description"], item["published"]])
-
-
-
-        # Download data after press submit button
         return response
 
-
     return render(request, "data_mining_app/data_preview.html", {"data": data})
-
-
-
-
-
-
