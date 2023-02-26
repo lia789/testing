c25e5d1b4209e02ec5ffa2a950caff3c2d0f87bd
lia789 <liahabib53@gmail.com>
2023-02-26 21:52:12 +0600

    __fiverr gigs content added

--- youtube_application/youtube_website/data_mining_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import channels_meta_data, data_preview
-
-
-urlpatterns = [
-    path("", channels_meta_data, name='home'),
-    path("data/", data_preview, name='data_preview_url'),
-]
