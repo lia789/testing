9b93cf8e8313669f9d757cc9795f4ac898b863f3
lia789 <liahabib53@gmail.com>
2023-04-27 20:43:37 +0600

    #

--- goodreads/data extraction/main.py
+++
@@ -1,4 +0,0 @@
-from scrapyd_api import ScrapydAPI
-
-scrapyd = ScrapydAPI('http://localhost:6800')
-scrapyd.schedule("good_reader_project", "love")
--- goodreads/project_good_reads/good_reader_project/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-
-class GoodReaderProjectItem(scrapy.Item):
-    quotes_text = scrapy.Field()
-    author_name = scrapy.Field()
-    number_of_likes = scrapy.Field()
